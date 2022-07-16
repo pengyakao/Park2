f8c59a65eb79f31ee8aad497bfee8c5c4bbd709e
“高鵬雅“ <pengyagao@gmail.com>
2022-07-16 13:31:55 +0800

    0716_ally

--- src/api/home/url.js
+++ src/api/home/url.js
@@ -1,6 +1,7 @@
 //教室
-export var url = "http://10.0.102.245:3001"
+// export var url = "http://10.0.102.245:3001"
 // 家裡
 // export var url = "http://192.168.0.101:3001"
-
+//臨時
+export var url = "http://192.168.31.114:3001"
 
--- src/api/storeApi.js
+++ src/api/storeApi.js
@@ -3,15 +3,7 @@ import axios from 'axios'
 // 設定 baseUrl
 const reqObj = axios.create({
     // baseURL 設定的是自己測試的 IP位址:server埠號 (ex. http://192.168.0.112:3000)
-    // <<<<<<< HEAD
-    // baseURL: 'http://192.168.0.46:3001',
-    // baseURL: 'http://10.0.102.113:3001',
-    // baseURL: 'http://172.20.10.4:3001',
-    baseURL: 'http://192.168.31.105:3001',
-    // =======
-    baseURL: 'http://192.168.100.11:3001',
-    // baseURL: 'http://10.0.102.113:3001',
-    // >>>>>>> 531cff1fd0b5b62c633324f8e3ddde00710de1ed
+    baseURL: "http://192.168.31.114:3001",
     header: {
         'Content-Type': 'application/json',
     },
